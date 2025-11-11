225e82b6ed1dfc7a9a7b96de8ed4bd15e45c166b
Fariz Wibisono <frzwibisono@gmail.com>
2025-11-11 20:25:18 +0700

    feat: add SearchResultsSkeleton and TicketListSkeleton exports to loading index

--- src/components/loading/index.ts
+++ src/components/loading/index.ts
@@ -2,3 +2,5 @@ export { LoadingSpinner } from '../LoadingSpinner';
 export { EventCardSkeleton } from './EventCardSkeleton';
 export { EventGridSkeleton } from './EventGridSkeleton';
 export { EventDetailSkeleton } from './EventDetailSkeleton';
+export { SearchResultsSkeleton } from './SearchResultsSkeleton';
+export { TicketListSkeleton } from './TicketListSkeleton';
